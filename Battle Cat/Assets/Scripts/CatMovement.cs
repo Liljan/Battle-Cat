414601fd5a3d2c2e8da6031becfc58a20e49994c
Liljan <marcus.t.lilja@gmail.com>
2017-06-02 18:52:02 +0200

    Basic walking.

---
+++ Battle Cat/Assets/Scripts/CatMovement.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CatMovement : MonoBehaviour
+{
+    // Movement variables
+    public float speed;
+
+    // Animation
+    private Animator animator;
+
+    public void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Movement();
+    }
+
+    public void Movement()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float y = Input.GetAxis("Vertical");
+
+        animator.SetBool("Walking", Mathf.Abs(x) > 0.0f || Mathf.Abs(y) > 0.0f);
+
+        transform.Translate(x * speed * Time.deltaTime, y * speed * Time.deltaTime, 0.0f);
+    }
+}

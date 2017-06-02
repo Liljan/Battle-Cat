5351fa1beb221c29db4e1fd1e47e5cfd82a36320
Liljan <marcus.t.lilja@gmail.com>
2017-06-02 19:07:49 +0200

    Turning player.

--- Battle Cat/Assets/Scripts/CatMovement.cs
+++ Battle Cat/Assets/Scripts/CatMovement.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -28,6 +29,20 @@ public class CatMovement : MonoBehaviour
 
         animator.SetBool("Walking", Mathf.Abs(x) > 0.0f || Mathf.Abs(y) > 0.0f);
 
+        SetFacingDirection(x);
+
         transform.Translate(x * speed * Time.deltaTime, y * speed * Time.deltaTime, 0.0f);
+    }
+
+    private void SetFacingDirection(float xAxis)
+    {
+        if (xAxis <= -0.1f)
+        {
+            transform.localScale = new Vector3(-1.0f, 1.0f, 1.0f);
+        }
+        else if (xAxis >= 0.1f)
+        {
+            transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
+        }
     }
 }
